40b022da5232d6cbf9dceb150227da4e57230d41
hasankh4n <hasan.khan@ucdconnect.ie>
2023-11-02 13:03:57 +0000

    Initial Commit

---
+++ command.java
@@ -0,0 +1,43 @@
+//Name: command
+//Author:
+//Version: 1.0
+//About: This file details how user inputs are taken and checked
+
+public class command {
+    
+    private enum CommandType {ROLL, QUIT};
+    private CommandType commandtype;
+
+    command(String command)
+    {
+        String commandFormatted = command.toUpperCase();
+
+        switch(commandFormatted){
+            case "Q":
+                commandtype = CommandType.QUIT;
+                break;
+            case "R":
+                commandtype = CommandType.ROLL;
+                break;
+            default:
+                commandtype = CommandType.ROLL;
+                break;
+        }
+    }
+
+       // User Input Check
+	public static boolean isValid (String command) 
+    {
+		String commandFormatted = command.toUpperCase();//valid input regardless of input case
+		return  commandFormatted.equals("Q") || commandFormatted.equals("R") ||
+				commandFormatted.matches("R[1-6]Q[1-6]");
+	}		
+
+    //User Input related methods
+    public boolean quit() {
+		return commandtype == CommandType.QUIT;
+	}
+
+
+
+}
---
+++ display.java
@@ -0,0 +1,15 @@
+//Name: display
+//Author: 
+//Version: 1.0
+//About: File detailing the logic behind displaying the board to user and text outputs
+
+import java.util.*;
+
+
+public class display {
+    
+    Scanner in;
+    command input;
+
+
+}
