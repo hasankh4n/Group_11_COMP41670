6a21921639f6df1b519adc5bd83238ee4306ced3
hasankh4n <hasan.khan@ucdconnect.ie>
2023-11-02 15:54:03 +0000

    Sprint 1 Functionality Check

--- backgammon.java
+++ backgammon.java
@@ -3,19 +3,30 @@ public class backgammon {
     public static void main (String... args){
         Board board = new Board();
         display display = new display();
+        Dice dice = new Dice();
         display.info();
 
         command command;
         do{
             // Display the current board state 
             display.getPlayerNames(board);
+            board.printBoard();
+            //Board.printBoard();
             boolean commandIn = false;
             do{
                 command = display.getUserInput();
                 if (command.quit())
                 {
                     commandIn = true;
-                } else {
+                } 
+                
+                else if (command.roll())
+                {
+                    dice.roll();
+                    dice.showFaces();
+                } 
+        
+                else {
                     display.displayCommandNotPossible();
                 }
 
