370b99d53b4df9b7b3602529e3338e2e9dd5341d
hasankh4n <hasan.khan@ucdconnect.ie>
2023-11-02 12:26:23 +0000

    Initial Commit

---
+++ board.java
@@ -0,0 +1,6 @@
+// VERSION : 1.0 
+// AUTHOR:
+// DESCRIPTION : This class contains the relevant methods for displaying the board
+public class board {
+    
+}
--- dice.java
+++ dice.java
@@ -1,1 +1,22 @@
-//testing
+// VERSION : 1.0 
+// AUTHOR:
+// DESCRIPTION : This class contains constructor for the dice , and generates the face values randomly
+
+import java.util.Random;
+
+public class dice{
+    
+    private int[] faceValue;
+    private Random rand;
+
+    
+
+
+
+
+
+
+
+}
+
+
