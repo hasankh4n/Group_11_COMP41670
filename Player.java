6141c69c0d37142a70828044294df26c58066e08
jamesduke488 <103256803+jamesduke488@users.noreply.github.com>
2023-11-16 20:54:57 +0000

    Add files via upload
    Added player class

---
+++ Player.java
@@ -0,0 +1,45 @@
+package BackgammonTest;
+
+public class Player {
+
+	private CounterType ctype;
+	private String colour;
+	
+	Player (int playerNumber) {
+		
+		if (playerNumber == 1) {
+			
+			ctype = CounterType.PLAYER1;
+			colour = ConsoleColors.RED_BACKGROUND;
+			
+		}
+		
+		else if (playerNumber == 2) {
+			
+			ctype = CounterType.PLAYER2;
+			colour = ConsoleColors.WHITE_BACKGROUND;
+			
+		}
+		
+		else {
+			
+			System.out.println("Player number invalid");
+			
+		}
+		
+		
+	}
+	
+	public CounterType getCounterType() {
+		
+		return ctype;
+		
+	}
+	
+public String getColour() {
+		
+		return colour;
+		
+	}
+	
+}
