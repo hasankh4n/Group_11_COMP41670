acbd3aacfd58bbdfb0a3990ad0209f0472e08957
jamesduke488 <103256803+jamesduke488@users.noreply.github.com>
2023-12-25 04:50:33 +0000

    Update ScoreTest.java

--- ScoreTest.java
+++ ScoreTest.java
@@ -2,6 +2,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Group Number: 11
+ * @author James Duke, Hasan Khan
+ * GitHub IDs: jamesduke488, hasankh4n
+ * @version 1.1
+ */
 
 class ScoreTest {
 
