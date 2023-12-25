6d4032134d888e45d42a14375be0bd440fb77f10
jamesduke488 <103256803+jamesduke488@users.noreply.github.com>
2023-12-25 02:42:15 +0000

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,6 +1,12 @@
 package BackgammonTest;
 import java.util.Random;
 
+/**
+ * Group Number: 11
+ * @author James Duke, Hasan Khan
+ * GitHub IDs: jamesduke488, hasankh4n
+ * @version 1.1
+ */
 
 public class Dice {
 
