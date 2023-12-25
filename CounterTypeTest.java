069a255a1357bacb220f30d60317997c61d46af7
jamesduke488 <103256803+jamesduke488@users.noreply.github.com>
2023-12-25 04:47:00 +0000

    Update CounterTypeTest.java

--- CounterTypeTest.java
+++ CounterTypeTest.java
@@ -2,6 +2,13 @@
 //tests enum constants are correctly initialized & toString() method behaves as expected
 // As enums are fixed, testing doesn't require extensive cases, as such it can be simple as here
 
+/**
+ * Group Number: 11
+ * @author James Duke, Hasan Khan
+ * GitHub IDs: jamesduke488, hasankh4n
+ * @version 1.1
+ */
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
